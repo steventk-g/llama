ce20b8f27b1b2363a37ffdac55260d00e430b26a
Liyang Lu <liyanglu@google.com>
2023-03-30 17:22:33 +0000

    refactor

--- example_tmp.py
+++ example_tmp.py
@@ -9,6 +9,7 @@ import fire
 import time
 import torch_xla.core.xla_model as xm
 import torch_xla.debug.metrics as met
+import torch_xla.debug.profiler as xp
 import json
 
 from pathlib import Path
@@ -20,6 +21,9 @@ def init(
     tokenizer_path: str,
     max_seq_len: int,
     max_batch_size: int,
+    dim: int = 4096,
+    n_layers: int = 32,
+    n_heads: int = 32,
 ) -> LLaMA:
     start_time = time.time()
     # checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
@@ -27,18 +31,21 @@ def init(
     # assert world_size == len(
     #     checkpoints
     # ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {world_size}"
-    # ckpt_path = checkpoints[local_rank]
+    # ckpt_path = checkpoints[rank]
     print("Loading")
     # checkpoint = torch.load(ckpt_path, map_location="cpu")
     # with open(Path(ckpt_dir) / "params.json", "r") as f:
     #     params = json.loads(f.read())
-
+    params = {"dim": dim,
+              "n_layers": n_layers,
+              "n_heads": n_heads,
+              }
     model_args: ModelArgs = ModelArgs(
-        max_seq_len=max_seq_len, max_batch_size=max_batch_size
+        max_seq_len=max_seq_len, max_batch_size=max_batch_size, **params
     )
     tokenizer = Tokenizer(model_path=tokenizer_path)
     model_args.vocab_size = tokenizer.n_words
-    #torch.set_default_tensor_type(torch.cuda.HalfTensor)  # TODO: this line puts the model to cuda device
+    # torch.set_default_tensor_type(torch.cuda.HalfTensor)  # TODO: this line puts the model to cuda device
     torch.set_default_tensor_type(torch.BFloat16Tensor)
     model = Transformer(model_args)
     device = xm.xla_device()
@@ -57,10 +64,14 @@ def main(
     top_p: float = 0.95,
     max_seq_len: int = 512,
     max_batch_size: int = 32,
+    dim: int = 4096,
+    n_layers: int = 32,
+    n_heads: int = 32,
 ):
+    server = xp.start_server(9012, only_on_master=False)
     torch.manual_seed(1)
     generator = init(
-        tokenizer_path, max_seq_len, max_batch_size
+        tokenizer_path, max_seq_len, max_batch_size, dim, n_layers, n_heads
     )
 
     prompts = [
@@ -90,17 +101,19 @@ def main(
 #
 #cheese =>""",
     ]
-    results = generator.generate(
-        prompts, max_gen_len=256, temperature=temperature, top_p=top_p
-    )
+    with torch.no_grad():
+        results = generator.generate(
+            prompts, max_gen_len=256, temperature=temperature, top_p=top_p
+        )
 
     for result in results:
         print(result)
         print("\n==================================\n")
 
-    results = generator.generate(
-        prompts, max_gen_len=256, temperature=temperature, top_p=top_p
-    )
+    with torch.no_grad():
+        results = generator.generate(
+            prompts, max_gen_len=256, temperature=temperature, top_p=top_p
+        )
 
     for result in results:
         print(result)
